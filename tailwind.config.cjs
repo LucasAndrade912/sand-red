0b5c56ff30bf35144a24733159e384cec61e3dd1
Lucas Andrade <lucasantos.dev@gmail.com>
2022-10-12 02:49:52 -0300

    configure fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.tsx'
+    './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: 10,
+      sm: 12,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 28,
+      '3xl': 32,
+      '4xl': 40
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Poppins, sans-serif',
+        sigmar: 'Sigmar One, cursive'
+      }
+    },
   },
   plugins: [],
 }
